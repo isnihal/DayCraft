73796ab8a39ff99904ea2b14335228de52c312f7
Nihal Ismail <nihalismailk@gmail.com>
2023-11-10 23:34:36 +0530

    feat: created timeline view

---
+++ DayCraft/Views/Views/TimeLineView.swift
@@ -0,0 +1,39 @@
+//
+//  TimeLineView.swift
+//  DayCraft
+//
+//  Created by Nihal Ismail on 11/10/23.
+//
+
+import SwiftUI
+
+struct TimeLineView: View {
+    var body: some View {
+        HStack(alignment: .top){
+            CircleLineView()
+            VStack(alignment: .leading,spacing: 0){
+                Text("08:00")
+                    .font(.title)
+                    .bold()
+                    .padding(.leading,4)
+                    .offset(CGSize(width: 0, height: -7))
+                DCCard()
+            }
+        }
+    }
+}
+
+struct CircleLineView: View {
+    var body: some View {
+        VStack(spacing: 0){
+            Circle()
+                .frame(height: 30)
+            Rectangle()
+                .frame(width: 2, height: 160)
+        }
+    }
+}
+
+#Preview {
+    TimeLineView()
+}
