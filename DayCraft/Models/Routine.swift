dd7b6d6506c5093364e98f810b27acd69be92215
Nihal Ismail <nihalismailk@gmail.com>
2023-11-16 00:44:46 +0530

    feat: added routine model

---
+++ DayCraft/Models/Routine.swift
@@ -0,0 +1,26 @@
+//
+//  Routine.swift
+//  DayCraft
+//
+//  Created by Nihal Ismail on 11/16/23.
+//
+
+import Foundation
+
+struct Routine: Identifiable{
+    let id: String
+    let title: String
+    let note: String
+    let time: Date
+    let days: Set<Weekday>
+}
+
+enum Weekday: Int{
+    case sunday = 1
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+}
